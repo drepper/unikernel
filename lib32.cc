7fc95082c35399a798ceec6403f5681e575a5d9f
Ulrich Drepper <drepper@gmail.com>
2019-02-28 17:50:54 +0100

    Some cleanup and WiP

--- lib32.cc
+++ lib32.cc
@@ -9,4 +9,77 @@ namespace lib32 {
     return i;
   }
 
+
+  cga_char* vidptr = (cga_char*) 0xb8000;
+
+  void clrscr()
+  {
+	  unsigned int j = 0;
+
+	  /* this loops clears the screen.
+	     There are 25 lines each of 80 columns; each element takes 2 bytes.  */
+	  while (j < 80 * 25) {
+	    /* blank character */
+	    vidptr[j] = (cga_char) { ' ', 0x07 };
+	    j = j + 1;
+	  }
+  }
+
+
+  void printat(unsigned x, unsigned y, const char* str)
+  {
+    auto* v = &vidptr[y * 80 + x];
+    while (*str)
+      (v++)->ch = *str++;
+  }
+
+
+  void printat(unsigned x, unsigned y, uint8_t attr, const char* str)
+  {
+    auto* v = &vidptr[y * 80 + x];
+    while (*str)
+      *(v++) = { *str++, attr };
+  }
+
+
+  void printat(unsigned x, unsigned y, unsigned long n)
+  {
+    auto* v = &vidptr[y * 80 + x];
+    char buf[2 * sizeof(n)];
+    char* cp = buf + 2 * sizeof(n);
+    if (n == 0)
+      *--cp = '0';
+    else
+      while (n > 0) {
+        *--cp = "0123456789abcdef"[n & 15];
+        n >>= 4;
+      }
+    while (cp < buf + 2 * sizeof(n))
+      (v++)->ch = *cp++;
+  }
+
+
+  void printat(unsigned x, unsigned y, uint8_t attr, unsigned long n)
+  {
+    auto* v = &vidptr[y * 80 + x];
+    char buf[2 * sizeof(n)];
+    char* cp = buf + 2 * sizeof(n);
+    if (n == 0)
+      *--cp = '0';
+    else
+      while (n > 0) {
+        *--cp = "0123456789abcdef"[n & 15];
+        n >>= 4;
+      }
+    while (cp < buf + 2 * sizeof(n))
+      *(v++) = { *cp++, attr };
+  }
+
+
+  void fail(const char* s)
+  {
+    lib32::printat(0, 0, 0x40, "FAIL: ");
+    lib32::printat(0, 6, 0x40, s);
+  }
+
 } // namespace lib32
--- lib32.h
+++ lib32.h
@@ -2,11 +2,36 @@
 #define _LIB32_h 1
 
 #include <cstddef>
+#include <cstdint>
 
 
 namespace lib32 {
 
   extern size_t strlen(const char* s);
+
+  // Screen handling.
+  struct cga_char {
+    char ch;
+    union {
+      uint8_t color;
+      struct {
+        uint8_t fg:4;
+        uint8_t bg:3;
+        uint8_t blink:1;
+      };
+    };
+  };
+
+  extern cga_char* vidptr;
+
+
+  extern void clrscr();
+  extern void printat(unsigned x, unsigned y, const char* str);
+  extern void printat(unsigned x, unsigned y, uint8_t attr, const char* str);
+  extern void printat(unsigned x, unsigned y, unsigned long n);
+  extern void printat(unsigned x, unsigned y, uint8_t attr, unsigned long n);
+
+  extern void fail(const char* s);
 
 } // namespace lib32
 
